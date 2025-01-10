5840e2310d975e33424c591386693d561bdb9d56
Blair Palmerlee <blairpalmerlee@gmail.com>
2025-01-09 18:04:39 -0800

    Initial commit

---
+++ CustomerApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Oly_Market
+ New giftcard database system to replace the wooden nickels used at the Olympia Farmer's Market. 
