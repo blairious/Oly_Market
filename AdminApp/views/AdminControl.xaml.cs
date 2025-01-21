32760a605a14302b7e69f3900fb6d937210504fe
Blair Palmerlee <blairpalmerlee@gmail.com>
2025-01-20 16:10:31 -0800

    Admin UI update.
    Continued frontend work for the admin windows application.

--- AdminApp/views/AdminControl.xaml.cs
+++ AdminApp/views/AdminControl.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AdminApp
+namespace AdminApp.views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AdminControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AdminControl : Window
     {
-        public MainWindow()
+        public AdminControl()
         {
             InitializeComponent();
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
     }
 }
