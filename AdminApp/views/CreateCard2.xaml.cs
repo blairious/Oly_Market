1dc3c84b8999cff7fb61e112d246b275320fc1ab
Blair Palmerlee <blairpalmerlee@gmail.com>
2025-02-03 22:25:53 -0800

    Created card output page
    Started work on the frontend new card output window.

---
+++ AdminApp/views/CreateCard2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AdminApp.views
+{
+    /// <summary>
+    /// Interaction logic for CreateCard2.xaml
+    /// </summary>
+    public partial class CreateCard2 : Window
+    {
+        public CreateCard2()
+        {
+            InitializeComponent();
+        }
+    }
+}
