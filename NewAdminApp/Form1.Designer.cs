7840d0d9ad13ec59c30191ee840ac4a173a60beb
Blair Palmerlee <blairpalmerlee@gmail.com>
2025-02-16 11:00:50 -0800

    Added new Admin App files

---
+++ NewAdminApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NewAdminApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(21, 82, 68);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(94, 449);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 53);
+            button1.TabIndex = 0;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(366, 572);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Farmers Market Admin";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+    }
+}
