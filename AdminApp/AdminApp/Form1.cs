abb3608efc22b5b31394b05ece467612dc852146
Blair Palmerlee <blairpalmerlee@gmail.com>
2025-02-16 22:45:53 -0800

    Finished login credential check
    Login window now queries a separate server and checks the retrieved hash against the authentication alg to either allow or disallow access.

--- AdminApp/AdminApp/Form1.cs
+++ AdminApp/AdminApp/Form1.cs
@@ -1,4 +1,5 @@
 using CozyHasher;
+using MySqlConnector;
 
 namespace AdminApp
 {
@@ -7,7 +8,6 @@ namespace AdminApp
         private string userName;
         private string passWord;
         Hasher hasher = new Hasher();
-
 
         public Form1()
         {
@@ -27,7 +27,7 @@ namespace AdminApp
         private void LoginButton_Click(object sender, EventArgs e)
         {
 
-            //Login syntax check. 
+            // Login syntax check. 
             if (userName == null || userName == "" || userName.Contains(' '))
             {
                 MessageBox.Show("Username missing or invalid.");
@@ -39,8 +39,39 @@ namespace AdminApp
                 return;
             }
 
+
+            // Connection and data fetch for login.
+            using var connection = new MySqlConnection("Server=10.0.0.191;User ID=blair;Password=OlyMarketTest;Database=OlyMarketGC");
+
+            connection.Open();
+
+            using var command = new MySqlCommand($"SELECT Password FROM Admin_Accounts WHERE AdminID = {userName};", connection);
+            using var reader = command.ExecuteReader();
+            string hashedWord = "";
+            bool AccessNow = false;
+            while (reader.Read())
+                hashedWord = reader.GetString(0);
+
+            connection.Close();
+
+            // Get boolean based on checking aquired hash and existence of user.
+            try
+            {
+                AccessNow = hasher.CheckHash(userName, passWord, hashedWord);
+            }
+            catch
+            {
+                AccessNow = false;
+            }
             
-            
+            if (AccessNow)
+            {
+                MessageBox.Show("Login Successful!");
+            }
+            else
+            {
+                MessageBox.Show("Login credentials incorrect.");
+            }
 
 
         }
